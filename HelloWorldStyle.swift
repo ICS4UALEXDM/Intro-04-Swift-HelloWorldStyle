1ca3935e4693972f000ba9f85953de2a73207eba
Alex De Meo <alexdm9572@gmail.com>
2023-02-09 18:44:00 +0000

    Styled

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,11 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Alex De Meo
+//  Created on 2023/02/08
+//  Version 1.0
+//  Copyright (c) 2023 Alex De Meo. All rights reserved.
+//
+//  The program tells the user my plans for next year.
+print("Next year I'm going to university")
+print("I am excited for University.")
